d3896de9b893bc255b59401ddc69706b94bd4031
Matthew Bicknell <mbickne1@emich.edu>
2022-06-18 23:47:32 -0400

    Bot Connects to the Discord Guild

--- client.py
+++ client.py
@@ -1,3 +1,14 @@
-import os
 import discord
-from dotenv import load_dotenv
+import utils
+
+def run():
+    client = discord.Client(intents=utils.intents)
+
+    @client.event
+    async def on_ready():
+        guild = discord.utils.get(client.guilds, name=utils.GUILD)
+        print(f'{client.user} has connected to Discord in the following guild:\n'
+            f'{guild.name}(id: {guild.id})')
+    
+    client.run(utils.TOKEN)
+
--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+#Imports
 import os
+import discord
 from dotenv import load_dotenv
-
+#Load Enviornment Variables
 load_dotenv()
-
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+#Create Intents
+#May need to change to less/more intents letter on need
+intents = discord.Intents.default()
+intents.members = True
